ea5a4463b034430a2c8f67a15e41ebc737028777
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-05-21 18:21:04 -0600

    add: Se cargo el proyecto KinalCodeQuest inicial

---
+++ KinalCodeQuest/src/main/java/com/J/M_CODERS/KinalCodeQuest/KinalCodeQuestApplication.java
@@ -0,0 +1,13 @@
+package com.J.M_CODERS.KinalCodeQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinalCodeQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinalCodeQuestApplication.class, args);
+	}
+
+}
