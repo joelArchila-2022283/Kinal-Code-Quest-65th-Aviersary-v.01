aa36e3435bd18c947475acb0543cd4298ea8b586
MiguelSantizo <msantizo-2022021@kinal.edu.gt>
2026-05-22 19:50:30 -0600

    refactor: se actualizo la clase

--- KinalCodeQuest/src/main/java/com/J/M_CODERS/KinalCodeQuest/service/impl/AreaTecnicaServiceImplements.java
+++ KinalCodeQuest/src/main/java/com/J/M_CODERS/KinalCodeQuest/service/impl/AreaTecnicaServiceImplements.java
@@ -1,7 +1,7 @@
 package com.J.M_CODERS.KinalCodeQuest.service.impl;
 
 import com.J.M_CODERS.KinalCodeQuest.model.entity.AreaTecnica;
-import com.J.M_CODERS.KinalCodeQuest.model.repository.AreaTecnicaRepository;
+import com.J.M_CODERS.KinalCodeQuest.repository.AreaTecnicaRepository;
 import com.J.M_CODERS.KinalCodeQuest.service.evaluator.AreaTecnicaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
