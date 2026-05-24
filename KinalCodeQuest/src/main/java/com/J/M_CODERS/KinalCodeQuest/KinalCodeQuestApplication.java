2d4924d9410da270c27a65dd4d0481457a439cbd
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-05-24 16:18:55 -0600

    feat: Se implemento el CommandLineRunner para que funcione la API

--- KinalCodeQuest/src/main/java/com/J/M_CODERS/KinalCodeQuest/KinalCodeQuestApplication.java
+++ KinalCodeQuest/src/main/java/com/J/M_CODERS/KinalCodeQuest/KinalCodeQuestApplication.java
@@ -1,13 +1,20 @@
 package com.J.M_CODERS.KinalCodeQuest;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KinalCodeQuestApplication {
+public class KinalCodeQuestApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KinalCodeQuestApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("----------------------------------------");
+		System.out.println(" KINAL CODE QUEST - SYSTEM ONLINE ");
+		System.out.println("----------------------------------------");
+	}
 }
