85e402743e326426a9ad1969b89f3f7e0c4885e9
MiguelSantizo <msantizo-2022021@kinal.edu.gt>
2026-06-14 21:43:18 -0600

    feat: se implemento la entidad PreguntaTrivia y endpoints del sistema de juego

--- KinalCodeQuest/src/main/java/com/J/M_CODERS/KinalCodeQuest/KinalCodeQuestApplication.java
+++ KinalCodeQuest/src/main/java/com/J/M_CODERS/KinalCodeQuest/KinalCodeQuestApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KinalCodeQuestApplication implements CommandLineRunner {
+public class
+KinalCodeQuestApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KinalCodeQuestApplication.class, args);
