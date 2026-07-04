898c4393d38ea043b9507db5fda7fb0123223267
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-07-04 10:06:00 -0600

    feat: se agrego la validación para que no se pueda hacer un bucle de puntos

--- KinalCodeQuest/src/main/java/com/J/M_CODERS/KinalCodeQuest/service/impl/ProgresoJugadorServiceImplements.java
+++ KinalCodeQuest/src/main/java/com/J/M_CODERS/KinalCodeQuest/service/impl/ProgresoJugadorServiceImplements.java
@@ -36,11 +36,13 @@ public class ProgresoJugadorServiceImplements implements ProgresoJugadorService 
 
     @Override
     public ProgresoJugador registrarIntento(ProgresoJugador progreso, String codigo, boolean esCorrecto) {
+        // Se incrementa siempre el contador de telemetría e historial para el Dashboard
         progreso.setIntentos(progreso.getIntentos() + 1);
         progreso.setUltimoCodigoEnviado(codigo);
 
         Jugador jugador = progreso.getJugador();
 
+        // CASO 1: El código es correcto y es la PRIMERA VEZ que completa la misión (Puntos Únicos)
         if (esCorrecto && !progreso.getCompletada()) {
             progreso.setCompletada(true);
             progreso.setFechaCompletado(LocalDateTime.now());
@@ -48,12 +50,14 @@ public class ProgresoJugadorServiceImplements implements ProgresoJugadorService 
             Mision m = progreso.getMision();
             jugador.setExperiencia(jugador.getExperiencia() + m.getExpRecompensa());
 
+            // Regla de Responsabilidad basada en la eficiencia de intentos
             if (progreso.getIntentos() <= 2) {
                 jugador.setPtosResponsabilidad(jugador.getPtosResponsabilidad() + 10);
             } else {
                 jugador.setPtosResponsabilidad(jugador.getPtosResponsabilidad() + 5);
             }
 
+            // Regla de Laboriosidad premiando la persistencia
             if (progreso.getIntentos() >= 4) {
                 jugador.setPtosLaboriosidad(jugador.getPtosLaboriosidad() + 15);
             } else {
@@ -61,10 +65,17 @@ public class ProgresoJugadorServiceImplements implements ProgresoJugadorService 
             }
 
             jugadorRepository.save(jugador);
-        } else if (!esCorrecto) {
+
+        }
+        // CASO 2: El código es incorrecto, pero la misión NO ha sido completada aún
+        // Evita el farmeo infinito de puntos de consolación enviando fallos intencionales
+        else if (!esCorrecto && !progreso.getCompletada()) {
             jugador.setPtosLaboriosidad(jugador.getPtosLaboriosidad() + 1);
             jugadorRepository.save(jugador);
         }
+
+        // CASO 3 (Implícito): Si la misión ya tiene 'progreso.getCompletada() == true',
+        // el sistema registra el nuevo código e intento, pero ignora la asignación de puntos.
 
         return progresoRepository.save(progreso);
     }
